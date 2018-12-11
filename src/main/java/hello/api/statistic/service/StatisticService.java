fce68419d76d0e8231f81eb15c41b8577ff952bf
kostya <zik1554@bk.ru>
2018-12-11 23:28:16 +0300

    first commit

---
+++ src/main/java/hello/api/statistic/service/StatisticService.java
@@ -0,0 +1,34 @@
+package hello.api.statistic.service;
+
+import hello.api.statistic.model.StatisticInfo;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
+
+public interface StatisticService {
+
+
+    @Nullable
+    void createStatistic(@Nonnull UUID uuid, @Nonnull String vk);
+
+    @Nullable
+    void updateUuidStat(@Nonnull UUID uuid, @Nonnull UUID newUuid);
+    @Nullable
+    void updateVKStat(@Nonnull StatisticInfo statisticInfo);
+
+    List<StatisticInfo> findWeekStatsByUUID(@Nonnull UUID uuid);
+
+    List<StatisticInfo> findMonthStatsByUUID(@Nonnull UUID uuid);
+
+    List<StatisticInfo> findAllStatsByUUID(@Nonnull UUID uuid);
+
+    StatisticInfo refreshStatistic(@Nonnull UUID uuid, @Nonnull String vk);
+
+
+    @Nullable
+    void deleteStats(@Nonnull UUID uuid);
+
+
+}
