b3a689ad41fb89f699d569f89fab079975e9b265
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-02-10 19:40:50 +0000

    456
    456

--- src/main/java/hello/api/statistic/web/StatisticController.java
+++ src/main/java/hello/api/statistic/web/StatisticController.java
@@ -1,5 +1,8 @@
 package hello.api.statistic.web;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.api.statistic.model.StatisticInfo;
 import hello.api.statistic.service.StatisticService;
 import org.slf4j.Logger;
@@ -8,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -22,9 +28,56 @@ public class StatisticController {
     @Autowired
     private StatisticService statRepos;
 
+    static final String ACCESS_TOKEN_VALIDATE_URI="https://api-my-gateway.herokuapp.com/api-gateway/oauth20/tokens/validate";
+    static final String CLIENT_ID="9aa3ae11759ae257e2f29484a32820845ae59763";
+    static final String CLIENT_SECRET="3997673d7814bbbcde139fe181e7fba723beb70a4e6e49363230ff78051f40d1";
+
+    private boolean OauthCheckToken(String token)
+    {
+
+        if(token==null)
+            return false;
+        else {
+            try {
+                String token2 = token.replace("Bearer ", "");
+                System.out.println(token2);
+                logger.info("oauth"+ token2);
+                logger.debug("oauth"+ token2);
+                UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(ACCESS_TOKEN_VALIDATE_URI)
+                        .queryParam("token", token2);
+                RestTemplate restTemplate = new RestTemplate();
+                String result = restTemplate.getForObject(builder.toUriString(), String.class);
+                logger.info("result"+ result);
+                logger.debug("result"+ result);
+                JsonFactory factory = new JsonFactory();
+
+                ObjectMapper mapper = new ObjectMapper(factory);
+                JsonNode rootNode = mapper.readTree(result);
+
+                Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
+
+                while (fieldsIterator.hasNext()) {
+
+                    Map.Entry<String, JsonNode> field = fieldsIterator.next();
+
+                    System.out.println(field.getKey());
+                    if (field.getKey() == "error") {
+                        return false;
+                    }
+
+                }
+                return  true;
+            } catch (Exception e) {
+                logger.error("oauth", e);
+                return  false;
+            }
+        }
+    }
 
     @GetMapping("/getAll{uuid}")
-    public ResponseEntity<List<StatisticInfo>> findAll(@RequestParam UUID uuid) {
+    public ResponseEntity<List<StatisticInfo>> findAll(@RequestHeader(value="Authorization",required = false) String token,@RequestParam UUID uuid) {
+        if(!OauthCheckToken(token))
+            return   new ResponseEntity(HttpStatus.UNAUTHORIZED);
         try {
             List<StatisticInfo> stats = statRepos.findAllStatsByUUID(uuid);
 
@@ -45,7 +98,10 @@ public class StatisticController {
 
 
     @PostMapping("/create")
-    public ResponseEntity createStat(@RequestBody Map<String, String> info) {
+    public ResponseEntity createStat(@RequestHeader(value="Authorization",required = false) String token,@RequestBody Map<String, String> info) {
+
+        if(!OauthCheckToken(token))
+            return   new ResponseEntity(HttpStatus.UNAUTHORIZED);
         try {
             statRepos.createStatistic(UUID.fromString(info.get("uid")), info.get("vk"));
             return new ResponseEntity(HttpStatus.CREATED);
@@ -55,7 +111,9 @@ public class StatisticController {
         }
     }
     @PutMapping("/updateUUID")
-    public ResponseEntity updateUuidUser(@RequestBody Map<String, String> requestUserDetails) {
+    public ResponseEntity updateUuidUser(@RequestHeader(value="Authorization",required = false) String token,@RequestBody Map<String, String> requestUserDetails) {
+        if(!OauthCheckToken(token))
+            return   new ResponseEntity(HttpStatus.UNAUTHORIZED);
         try {
 
 
@@ -70,7 +128,9 @@ public class StatisticController {
     }
 
     @GetMapping("/get{vk}{uuid}")
-    public ResponseEntity<StatisticInfo> getStat(@RequestParam String vk, @RequestParam UUID uuid) {
+    public ResponseEntity<StatisticInfo> getStat(@RequestHeader(value="Authorization",required = false) String token,@RequestParam String vk, @RequestParam UUID uuid) {
+        if(!OauthCheckToken(token))
+            return   new ResponseEntity(HttpStatus.UNAUTHORIZED);
         try {
 
             return new ResponseEntity(statRepos.refreshStatistic(uuid, vk), HttpStatus.OK);
@@ -81,8 +141,9 @@ public class StatisticController {
     }
 
     @DeleteMapping("/delete{uuid}")
-    public ResponseEntity deleteStats(@RequestParam UUID uuid) {
-
+    public ResponseEntity deleteStats(@RequestHeader(value="Authorization",required = false) String token,@RequestParam UUID uuid) {
+        if(!OauthCheckToken(token))
+            return   new ResponseEntity(HttpStatus.UNAUTHORIZED);
         try {
             statRepos.deleteStats(uuid);
 
